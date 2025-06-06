4cb1f0384de0d2cc17f246bb32dc23eb40cb9e96
Kennedy Ashley Johnson <kenn.john320@gmail.com>
2025-06-05 23:14:03 -0700

    Added more parts to the app.py

--- ai_generator.py
+++ ai_generator.py
@@ -1,1 +1,15 @@
+# Get the OpenAPI SDK
 import openai
+import os
+from dotenv import load_dotenv
+
+# Get env 
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+def generate_blog(keyword, seo_metrics):
+    
+    
+    
+    
+    return "wompwomp"
--- app.py
+++ app.py
@@ -3,15 +3,27 @@
 
 # Basic template for what I need
 from flask import Flask
+from seo_fetcher import random_metrics
 
 
 app = Flask(__name__)
 
-@app.route('')
-def create(): 
-    return 'Created!'
+@app.route('/generate', methods=["GET"])
+def generate(): 
+    keyword = request.args.get("keyword")
+    
+    if not keyword:
+        return jsonify({"error":"Keyword is required "}), 400
+
+    seo_metrics = random_metrics(keyword)
+    blog_post = generate_blog(keyword, seo_metrics)
 
 
+    return jsonify({
+        "keyword": keyword,
+        "seo": seo_metrics,
+        "content": blog_post
+        })
 
 if __name__ == '__main__':
     app.run(debug=True)
